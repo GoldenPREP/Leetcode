fecd3e4275ba277c31ab7f849565a4b8fee849bc
GoldenPREP <87166356+GoldenPREP@users.noreply.github.com>
2025-06-26 12:20:30 +0300

    Update Leetcode2.addTwoNumbers.cpp
    removed redundant variables, and fixed adding/sum logic.

--- Leetcode2.addTwoNumbers.cpp
+++ Leetcode2.addTwoNumbers.cpp
@@ -29,9 +29,7 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
     {
         ListNode* temp = new ListNode(); 
-        ListNode* result = temp;
-        int l1s = 0; 
-        int l2s = 0; 
+        ListNode* result = temp; 
         int sum = 0; 
         int carry = 0;  
         //read each value then add to l1 int by adding to the variable 
@@ -39,26 +37,23 @@ public:
         //number in the next place ex: 2 x 1 = 2 + 4 x 10 = 
         // 42 + 3 x 100 = 142 example was [2,4,1]
         while(l1 != nullptr || l2 != nullptr || carry != 0) 
-        { 
+        {  
+            sum = carry;
             if (l1) 
             { 
-                l1s += (l1 != nullptr) ? (l1->val) : 0; 
+                sum += l1->val; 
                 l1 = l1->next;
             }
             if (l2)
             {
-                l2s += (l2 != nullptr) ? (l2->val) : 0;
+                sum += l2->val;
                 l2 = l2->next;  
             } 
-            int sum = l1s + l2s + carry;
             carry = sum / 10; 
-            int total = sum % 10; 
-            temp->next = new ListNode(total); 
+            temp->next = new ListNode(sum % 10); 
             temp = temp->next;
         }  
-        ListNode* res = result->next;
-        delete result;
-        return res;
+        return result->next;
     }  
   
 };
